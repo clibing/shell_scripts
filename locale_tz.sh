7e134e82ffad78b7392df2b885a267533e82aa45
liuboxun <liuboxun@gozap.com>
2018-11-06 16:19:08 +0800

    fix(locale tz): support centos
    support centos
    
    Signed-off-by: liuboxun <liuboxun@gozap.com>

--- locale_tz.sh
+++ locale_tz.sh
@@ -9,14 +9,26 @@ set -e
 TZ='Asia/Shanghai'
 
 function setlocale(){
-    locale-gen --purge en_US.UTF-8 zh_CN.UTF-8
-    echo 'LANG="en_US.UTF-8"' > /etc/default/locale
-    echo 'LANGUAGE="en_US:en"' >> /etc/default/locale
+    if [ -f "/etc/centos-release" ];then
+        echo "centos..."
+        cat > /etc/locale.conf <<EOF
+LANG="zh_CN.UTF-8"
+EOF
+    else
+        echo "ubuntu..."
+        locale-gen --purge en_US.UTF-8 zh_CN.UTF-8
+        echo 'LANG="en_US.UTF-8"' > /etc/default/locale
+        echo 'LANGUAGE="en_US:en"' >> /etc/default/locale
+    fi
+
 }
 
 function settimezone(){
     ln -sf /usr/share/zoneinfo/${TZ} /etc/localtime
     echo ${TZ} > /etc/timezone
+    if [ -f "/etc/centos-release" ];then
+        timedatectl set-timezone Asia/Shanghai
+    fi
 }
 
 setlocale
