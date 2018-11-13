ffbb6ad752ed335a6ab04f3026fcdd0be5084dd2
liuboxun <liuboxun@gozap.com>
2018-11-13 14:45:01 +0800

    fix(apt): ubuntu apt init shell
    ubuntu apt init shell
    
    Signed-off-by: liuboxun <liuboxun@gozap.com>

--- apt_sources.sh
+++ apt_sources.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-function apt_source(){
+function apt_tuna(){
 	cp /etc/apt/sources.list /etc/apt/sources.list.bak
 	cat >/etc/apt/sources.list <<EOF
 # deb cdrom:[Ubuntu 16.04 LTS _Xenial Xerus_ - Release amd64 (20160420.1)]/ xenial main restricted
@@ -19,6 +19,75 @@ EOF
 
 }
 
-apt_source
+function apt_dongbei(){
+	cp /etc/apt/sources.list /etc/apt/sources.list.bak
+	cat >/etc/apt/sources.list <<EOF
+deb-src http://mirror.neu.edu.cn/ubuntu/ xenial main restricted #Added by software-properties
+deb http://mirror.neu.edu.cn/ubuntu/ xenial main restricted
+deb-src http://mirror.neu.edu.cn/ubuntu/ xenial restricted multiverse universe #Added by software-properties
+deb http://mirror.neu.edu.cn/ubuntu/ xenial-updates main restricted
+deb-src http://mirror.neu.edu.cn/ubuntu/ xenial-updates main restricted multiverse universe #Added by software-properties
+deb http://mirror.neu.edu.cn/ubuntu/ xenial universe
+deb http://mirror.neu.edu.cn/ubuntu/ xenial-updates universe
+deb http://mirror.neu.edu.cn/ubuntu/ xenial multiverse
+deb http://mirror.neu.edu.cn/ubuntu/ xenial-updates multiverse
+deb http://mirror.neu.edu.cn/ubuntu/ xenial-backports main restricted universe multiverse
+deb-src http://mirror.neu.edu.cn/ubuntu/ xenial-backports main restricted universe multiverse #Added by software-properties
+deb http://archive.canonical.com/ubuntu xenial partner
+deb-src http://archive.canonical.com/ubuntu xenial partner
+deb http://mirror.neu.edu.cn/ubuntu/ xenial-security main restricted
+deb-src http://mirror.neu.edu.cn/ubuntu/ xenial-security main restricted multiverse universe #Added by software-properties
+deb http://mirror.neu.edu.cn/ubuntu/ xenial-security universe
+deb http://mirror.neu.edu.cn/ubuntu/ xenial-security multiverse
+EOF
+
+}
+
+function apt_aliyun(){
+	cp /etc/apt/sources.list /etc/apt/sources.list.bak
+	cat >/etc/apt/sources.list <<EOF
+# deb cdrom:[Ubuntu 16.04 LTS _Xenial Xerus_ - Release amd64 (20160420.1)]/ xenial main restricted
+deb-src http://archive.ubuntu.com/ubuntu xenial main restricted #Added by software-properties
+deb http://mirrors.aliyun.com/ubuntu/ xenial main restricted
+deb-src http://mirrors.aliyun.com/ubuntu/ xenial main restricted multiverse universe #Added by software-properties
+deb http://mirrors.aliyun.com/ubuntu/ xenial-updates main restricted
+deb-src http://mirrors.aliyun.com/ubuntu/ xenial-updates main restricted multiverse universe #Added by software-properties
+deb http://mirrors.aliyun.com/ubuntu/ xenial universe
+deb http://mirrors.aliyun.com/ubuntu/ xenial-updates universe
+deb http://mirrors.aliyun.com/ubuntu/ xenial multiverse
+deb http://mirrors.aliyun.com/ubuntu/ xenial-updates multiverse
+deb http://mirrors.aliyun.com/ubuntu/ xenial-backports main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ xenial-backports main restricted universe multiverse #Added by software-properties
+deb http://archive.canonical.com/ubuntu xenial partner
+deb-src http://archive.canonical.com/ubuntu xenial partner
+deb http://mirrors.aliyun.com/ubuntu/ xenial-security main restricted
+deb-src http://mirrors.aliyun.com/ubuntu/ xenial-security main restricted multiverse universe #Added by software-properties
+deb http://mirrors.aliyun.com/ubuntu/ xenial-security universe
+deb http://mirrors.aliyun.com/ubuntu/ xenial-security multiverse
+EOF
+
+}
+
+
+TYPE=${type:-"tuna"}
+
+while getopts "t:a:d" OPT; do
+    case $OPT in
+        t)
+            TYPE=$OPTARG;;
+        a)
+            TYPE=$OPTARG;;
+        d)
+            TYPE=$OPTARG;;
+    esac
+done
+
+if [ "${TYPE}" = "tuna" ]; then
+	apt_tuna
+elif [ "${TYPE}" = "dongbei" ]; then
+	apt_dongbei
+elif [ "${TYPE}" = "aliyun" ]; then
+	apt_aliyun
+fi
 apt update
 apt upgrade
